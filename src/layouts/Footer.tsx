ac52b66f12f4bb8086488cd185d995abdc22807f
TrulyItzIcyGaming <Brandonrsn0502v2@gmail.com>
2025-01-07 01:11:58 -0500

    Initial commit

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -18,42 +18,6 @@ const Footer = () => {
             . 🚀
             <span class="text"><a id="linkanimation" href="https://stacksorted.com/links/jtb-studios" target="_blank">Stack sorted.</a> </span>
           </p>
-          <style>
-          .text {
-  font-size: 25px;
-}
-#linkanimation {
-  text-decoration: none;
-  text-decoration-color: none;
-  color: white;
-  position: relative;
-}
-#linkanimation::before {
-  position: absolute;
-  content: "";
-  width: 100%;
-  height: 1px;
-  background-color: #FEFEFE;
-  transform: scale(1,1);
-  transition: background-color .5s ease-in-out;
-  bottom: 0px;
-}
-#linkanimation:hover::before {
-  animation: link ease 1s 1 300ms;
-  transform-origin: right;  
-}
-@keyframes link {
-  50%{
-    transform: scaleX(0);
-  }
-  50.1% {
-    transform: translateX(-100%) scalex( -0.01);   
-  }
-  100% {
-    transform: translateX(-100%) scalex(-1);
-  }
-}
-          </style>
         </div>
       </footer>
     </>
